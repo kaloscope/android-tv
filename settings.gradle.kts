6dc081334631002bfbc3c280fcd9c7a0bafb516e
Xylitol <jam.xylitol@gmail.com>
2026-07-31 15:58:46 +0800

    build: migrate to AGP 9.3.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,4 @@
 pluginManagement {
-    buildscript {
-        repositories {
-            mavenCentral()
-            maven {
-                url = uri("https://storage.googleapis.com/r8-releases/raw")
-            }
-        }
-        dependencies {
-            // Kotlin 2.4 metadata requires R8 9.1.29 or newer.
-            classpath("com.android.tools:r8:9.1.29")
-        }
-    }
     repositories {
         google()
         mavenCentral()
